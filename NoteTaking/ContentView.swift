03e1196abe0532e8813c64776ad5cefa448a0931
higracehuang <higracehuang@gmail.com>
2024-01-18 14:15:22 -0600

    Refactor

--- NoteTaking/ContentView.swift
+++ NoteTaking/ContentView.swift
@@ -5,12 +5,6 @@ struct ContentView: View {
   @Environment(\.managedObjectContext) private var viewContext
   
   @FetchRequest(
-    sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
-    animation: .default)
-  private var items: FetchedResults<Item>
-  
-  
-  @FetchRequest(
     sortDescriptors: [NSSortDescriptor(keyPath: \NoteEntry.createdAt, ascending: true)], animation: .default)
   private var noteEntries: FetchedResults<NoteEntry>
   
@@ -30,50 +24,15 @@ struct ContentView: View {
             }
           }
         }
-        .onDelete(perform: deleteItems)
       }
       .toolbar {
         ToolbarItem {
-          Button(action: addNoteEntry) {
+          Button(action: PersistenceController.shared.addNoteEntry) {
             Label("Add Note", systemImage: "plus")
           }
         }
       }
       Text("Select a note")
-    }
-  }
-
-  
-  private func addNoteEntry() {
-    withAnimation {
-      let newNoteEntry = NoteEntry(context: viewContext)
-      
-      newNoteEntry.createdAt = Date()
-      newNoteEntry.updatedAt = Date()
-      newNoteEntry.title = "Untitled"
-      newNoteEntry.content = "TBD"
-      
-      do {
-        try viewContext.save()
-      } catch {
-        let nsError = error as NSError
-        fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-      }
-    }
-  }
-  
-  private func deleteItems(offsets: IndexSet) {
-    withAnimation {
-      offsets.map { items[$0] }.forEach(viewContext.delete)
-      
-      do {
-        try viewContext.save()
-      } catch {
-        // Replace this implementation with code to handle the error appropriately.
-        // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-        let nsError = error as NSError
-        fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-      }
     }
   }
 }
